1d887acecd21f114c8a82e0ff42d126bcad483aa
unknown <xurshedergashboyev@gmail.com>
2020-11-05 23:04:28 +0500

    TodoItem

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,11 +1,22 @@
 import React, { Component } from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faPen, faPenAlt, faTrash, faTrashAlt, faTrashRestore } from '@fortawesome/free-solid-svg-icons'
 
 export default class TodoItem extends Component {
     render() {
+        const {title, handleDelete, handleEdit} = this.props
         return (
-            <div>
-                hello from todo item
-            </div>
+            <li className="list-group-item text-capitalize d-flex justify-content-between my-2">
+                <h6>{title}</h6>
+                <div className="todo-icon">
+                    <span className="mx-2 text-success" onClick={handleEdit}>
+                        <FontAwesomeIcon icon={faPenAlt} />
+                    </span>
+                    <span className="mx-2 text-danger" onClick={handleDelete}>
+                        <FontAwesomeIcon icon={faTrashAlt} />
+                    </span>
+                </div>
+            </li>
         )
     }
 }
